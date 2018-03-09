62cfae79cfcb91482ce7242179448f87472c72c2
AntonYakovenko <antonyakovenko96@gmail.com>
2018-03-09 18:25:04 +0200

    configured mySql db, boneCP

--- src/main/java/com/company/training_portal/config/AppConfig.java
+++ src/main/java/com/company/training_portal/config/AppConfig.java
@@ -1,10 +1,12 @@
 package com.company.training_portal.config;
 
+import com.jolbox.bonecp.BoneCPDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -18,16 +20,26 @@ import javax.sql.DataSource;
 public class AppConfig {
 
     @Bean
-    public DataSource h2DataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .build();
+    public DataSource boneCPDataSource() {
+        BoneCPDataSource dataSource = new BoneCPDataSource();
+        dataSource.setDriverClass("com.mysql.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/training_portal_db");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
     }
+
+//    @Bean
+//    public DataSource h2DataSource() {
+//        return new EmbeddedDatabaseBuilder()
+//                .setType(EmbeddedDatabaseType.H2)
+//                .build();
+//    }
 
     @Bean
     public PlatformTransactionManager transactionManager() {
         DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
-        transactionManager.setDataSource(h2DataSource());
+        transactionManager.setDataSource(boneCPDataSource());
         return transactionManager;
     }
 }
--- src/main/java/com/company/training_portal/model/enums/StudentQuizStatus.java
+++ src/main/java/com/company/training_portal/model/enums/StudentQuizStatus.java
@@ -1,6 +1,7 @@
 package com.company.training_portal.model.enums;
 
 public enum StudentQuizStatus {
+    // todo: create status 'CLOSED' when all students in group have status 'FINISHED'
     OPENED("OPENED"), PASSED("PASSED"), FINISHED("FINISHED");
 
     private String studentQuizStatus;

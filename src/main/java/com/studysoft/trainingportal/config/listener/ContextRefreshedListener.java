014eafddb79aa4bd7fad4bd3bc00fbf7e4e0f919
artemyakovenko <jeep2801@gmail.com>
2019-05-01 18:25:20 +0300

    fix bug with Long comparison

--- src/main/java/com/studysoft/trainingportal/config/listener/ContextRefreshedListener.java
+++ src/main/java/com/studysoft/trainingportal/config/listener/ContextRefreshedListener.java
@@ -6,7 +6,10 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 
 @Component
@@ -18,7 +21,38 @@ public class ContextRefreshedListener {
     public void onContextRefreshed(ContextRefreshedEvent event) {
         ApplicationContext context = event.getApplicationContext();
         Environment env = context.getEnvironment();
-        log.info("Context with name = '" + context.getDisplayName() + "' successfully initialized");
-        log.info("Active profiles: " + Arrays.toString(env.getActiveProfiles()));
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (contextPath == null || contextPath.isEmpty()) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info(String.format("\n----------------------------------------------------------\n\t" +
+                        "Application '%s' is running! Access URLs:\n\t" +
+                        "Local: \t\t%s://localhost:%s%s\n\t" +
+                        "External: \t%s://%s:%s%s\n\t" +
+                        "Profile(s): \t%s\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                Arrays.toString(env.getActiveProfiles())));
     }
 }
